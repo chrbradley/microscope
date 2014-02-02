1de6d47aed769fcf1b354c5e787e576d97a1d3c9
Chris <chrbradley@gmail.com>
2014-02-02 14:07:51 -0500

    4-3 wired collection into postsList template

--- server/fixtures.js
+++ server/fixtures.js
@@ -1,18 +1,18 @@
 if (Posts.find().count() === 0) {
   Posts.insert({
-    title: 'Introducing Telescope',
+    title: 'Introducing Telescope - Fixture',
     author: 'Sacha Greif',
     url: 'http://sachagreif.com/introducing-telescope/'
   });
 
   Posts.insert({
-    title: 'Meteor',
+    title: 'Meteor - Fixture',
     author: 'Tom Coleman',
     url: 'http://meteor.com'
   });
 
   Posts.insert({
-    title: 'The Meteor Book',
+    title: 'The Meteor Book - Fixture',
     author: 'Tom Coleman',
     url: 'http://themeteorbook.com'
   });
