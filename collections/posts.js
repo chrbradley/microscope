0840131dd945c34a296bafb3d957adcdf5d72f5c
Chris <chrbradley@gmail.com>
2014-02-02 21:56:33 -0500

    7-2 removed insecure and allowed certain write to posts

--- collections/posts.js
+++ collections/posts.js
@@ -1,3 +1,10 @@
 // omitting var keyword here to make the posts collection
 // available to the whole application
 Posts = new Meteor.Collection('posts');
+
+Posts.allow({
+	insert: function(userId, doc) {
+		//only allow posting if you are logged in
+		return !! userId;
+	}
+});
