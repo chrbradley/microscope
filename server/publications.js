db2c05c181aefba1f33ab95321736707d7a3c88c
chrbradley <chrbradley@gmail.com>
2014-02-04 11:21:38 -0500

    11-2 Display notifications in the header.

--- server/publications.js
+++ server/publications.js
@@ -2,10 +2,10 @@ Meteor.publish('posts', function() {
   return Posts.find();
 });
 
-Meteor.publish('comments',function(postId) {
-	return Comments.find({postId: postId});
+Meteor.publish('comments', function(postId) {
+  return Comments.find({postId: postId});
 });
 
 Meteor.publish('notifications', function() {
-  return Notifications.find();
+  return Notifications.find({userId: this.userId});
 });
