ebf706596185c9f7c6dec86ac2fd3dafbf3a4c4a
Chris <chrbradley@gmail.com>
2014-02-05 21:58:47 -0500

    13-1 added basic upvoting alogrithm.

--- client/views/posts/post_item.js
+++ client/views/posts/post_item.js
@@ -2,11 +2,16 @@ Template.postItem.helpers({
   ownPost: function() {
     return this.userId == Meteor.userId();
   },
-  
   domain: function() {
     var a = document.createElement('a');
     a.href = this.url;
     return a.hostname;
   }
-  
+});
+
+Template.postItem.events({
+  'click .upvote': function(e) {
+    e.preventDefault();
+    Meteor.call('upvote', this._id);
+  }
 });
--- collections/posts.js
+++ collections/posts.js
@@ -1,5 +1,3 @@
-// omitting var keyword here to make the posts collection
-// available to the whole application
 Posts = new Meteor.Collection('posts');
 
 Posts.allow({
@@ -9,8 +7,8 @@ Posts.allow({
 
 Posts.deny({
   update: function(userId, post, fieldNames) {
-    //may only edit the following two fields:
-    return(_.without(fieldNames, 'url', 'title').length > 0);
+    // may only edit the following two fields:
+    return (_.without(fieldNames, 'url', 'title').length > 0);
   }
 });
 
@@ -18,42 +16,53 @@ Meteor.methods({
   post: function(postAttributes) {
     var user = Meteor.user(),
       postWithSameLink = Posts.findOne({url: postAttributes.url});
-
+    
     // ensure the user is logged in
     if (!user)
       throw new Meteor.Error(401, "You need to login to post new stories");
-
+    
     // ensure the post has a title
     if (!postAttributes.title)
       throw new Meteor.Error(422, 'Please fill in a headline');
-
+    
     // check that there are no previous posts with the same link
     if (postAttributes.url && postWithSameLink) {
       throw new Meteor.Error(302, 
         'This link has already been posted', 
         postWithSameLink._id);
     }
-
+    
     // pick out the whitelisted keys
-    var post = _.extend(_.pick(postAttributes, 'url', 'message'), {
-      title: postAttributes.title + (this.isSimulation ? '(client)' : '(server)'),
+    var post = _.extend(_.pick(postAttributes, 'url', 'title', 'message'), {
       userId: user._id, 
       author: user.username, 
       submitted: new Date().getTime(),
-      commentsCount: 0
+      commentsCount: 0,
+      upvoters: [],
+      votes: 0
     });
-
-    // wait 5 seconds
-    if (! this.isSimulation) {
-      var Future = Npm.require('fibers/future');
-      var future = new Future();
-      Meteor.setTimeout(function() {
-        future.return();
-      }, 5 * 1000);
-      future.wait();
-    }
-
+    
     var postId = Posts.insert(post);
+    
     return postId;
+  },
+  
+  upvote: function(postId) {
+    var user = Meteor.user();
+    // ensure the user is logged in
+    if (!user)
+      throw new Meteor.Error(401, "You need to login to upvote");
+    
+    var post = Posts.findOne(postId);
+    if (!post)
+      throw new Meteor.Error(422, 'Post not found');
+    
+    if (_.include(post.upvoters, user._id))
+      throw new Meteor.Error(422, 'Already upvoted this post');
+    
+    Posts.update(post._id, {
+      $addToSet: {upvoters: user._id},
+      $inc: {votes: 1}
+    });
   }
 });
