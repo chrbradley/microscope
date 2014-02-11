ba5cf568cad119ed6f411558773f6a56a273b78d
chrbradley <chrbradley@gmail.com>
2014-02-11 09:33:08 -0500

    16-6 Added Most Clicked view.

--- packages/bitly/bitly.js
+++ packages/bitly/bitly.js
@@ -23,7 +23,7 @@ Bitly.shortenURL = function(url){
 }
 
 Bitly.getClicks = function(link) {
-  if(!Meteor.setting.bitly)
+  if(!Meteor.settings.bitly)
     throw new Meteor.Error(500, 'Please provide a Bitly token in Meteor.settings');
 
   var statsResponse = Meteor.http.get(
@@ -46,3 +46,20 @@ Meteor.methods({
     return Bitly.getClicks(link);
   }
 })
+
+var callInterval = 10000; // 1000ms * 10 = 10s
+Meteor.setInterval(function(){
+  // get all posts with the shortUrl property
+  var shortUrlPosts = Posts.find({shortUrl: { $exists: true }});
+  var postsNumber = shortUrlPosts.count();
+  // initialize counter
+  var count = 0;
+  shortUrlPosts.forEach(function(post){
+    // calculate the right delay to distribute API calls evenly throughout the interval
+    var callTimeout = Math.round(callInterval/postsNumber*count);
+    Meteor.setTimeout(function(){
+      Posts.update(post._id, {$set: {clicks: Bitly.getClicks(post.shortUrl)}});
+    }, callTimeout);
+    count++;
+  });
+}, callInterval);
