fd5b7f8f5bc72e7713165186e337dd2f635092c5
chrbradley <chrbradley@gmail.com>
2014-02-12 10:47:37 -0500

    17-4 displayed intercom inbox.

--- client/main.js
+++ client/main.js
@@ -4,6 +4,12 @@ Deps.autorun(function(){
       email: Meteor.user().emails[0].address,
       created_at: Math.round(Meteor.user().createdAt/1000),
       user_name: Meteor.user().username,
+      user_id: Meteor.user()._id,
+      user_hash: Meteor.user().intercomHash,
+      widget: {
+        activator: "#Intercom",
+        use_counter: true
+      },
       app_id: "9b593f5995da6d2bd8f418754c0411d55201258a"
     };
     Intercom('boot', intercomSettings);
--- packages/intercom/package.js
+++ packages/intercom/package.js
@@ -3,5 +3,7 @@ Package.describe({
 });
 
 Package.on_use(function (api){
-	api.add_files('intercom_loader.js', 'client');
+	api.add_files( 'intercom_loader.js', 'client' );
+  api.add_files( 'intercom_server.js', 'server' );
+  api.export( 'IntercomHash', 'server' );
 });
