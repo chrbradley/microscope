d523806d20b1db60e0a7680d429d7b08977a021c
chrbradley <chrbradley@gmail.com>
2014-02-13 20:30:36 -0500

    17-1-2 got intercom working

--- client/main.js
+++ client/main.js
@@ -1,7 +1,9 @@
-window.intercomSettings = {
-  // TODO: The current logged in user's email address.
-  email: "john.doe@example.com",
-  // TODO: The current logged in user's sign-up date as a Unix timestamp.
-  created_at: 1234567890,
-  app_id: "9b593f5995da6d2bd8f418754c0411d55201258a"
-};
+  window.intercomSettings = {
+    // TODO: The current logged in user's full name
+    name: "John Doe",
+    // TODO: The current logged in user's email address.
+    email: "john.doe@example.com",
+    // TODO: The current logged in user's sign-up date as a Unix timestamp.
+    created_at: 1234567890,
+    app_id: "5016e18208eec71930649c46a1cd29689cd6db42"
+  };
